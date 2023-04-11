76385ed8b87a9c7b13d71d021fa3a0a4070acdde
李潇怡 <lixiaoyi@lixiaoyideMacBook-Pro.local>
2023-04-10 23:09:32 -0400

    get lat lon

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import sqlite3
+import urllib
 import matplotlib.pyplot as plt
 
 def load_json(filename):
@@ -12,7 +13,6 @@ def load_json(filename):
 
     except:
         d = {}
-    # print(d)
     return d
 
 
@@ -24,25 +24,38 @@ def write_json(filename, dict):
 def get_api(url, params):
     r = requests.get(url, params)
     health_json = r.json()
-    # health_data = json.load(r.content)
     return health_json
 
 
-def cache_health_data(filename):
-    health_dic = []
-    who_url = "https://chronicdata.cdc.gov/resource/cj8b-94cj.json"
-    health_list = get_api(who_url, None)
-    write_json(filename, health_list)
-
-
-def get_lat(filename):
-    loc_list = load_json(filename)
-    coor_list = []
+def get_lat(f_r, f_w):
+    loc_list = load_json(f_r)
+    state_d = {}
     for region_dic in loc_list:
+        state = region_dic["stateabbr"]
+        city = region_dic["placename"]
         coor = region_dic["geolocation"]["coordinates"]
-        coor_rounded = round(coor, 6)
-        coor_list.append()
-    return coor_list     # a list
+        pop = int(region_dic["totalpopulation"])
+        
+        d = {}
+        coor_p = []
+        for i in coor:
+            coor_rounded = round(i, 2)
+            coor_p.append(coor_rounded)
+        d[city] = (pop, coor_p)
+        if state not in state_d.keys():
+            state_d[state] = d
+        else:
+            state_d[state].update(d)
+
+    two_city_d = {}
+    for state in state_d:
+        new_d = {}
+        s_city = sorted(state_d[state].items(), key = lambda x:x[1], reverse=True)
+        new_d[s_city[0][0]] = s_city[0][1][1]
+        if len(s_city) > 1:
+            new_d[s_city[1][0]] = s_city[1][1][1]
+        two_city_d[state] = new_d
+    write_json(f_w, two_city_d)
 
 
 def cache_weather(coor_list, filename):
@@ -50,16 +63,14 @@ def cache_weather(coor_list, filename):
     base = "https://api.openweathermap.org/data/3.0/onecall/timemachine"
     # for i in range(25):
     for i in coor_list:
-        dic = {"lat":i[0], "lon":i[1], "dt":"2000", "appid":"0ed702778efe831f0e3d546dc34eece3"}
+        dic = {"lat":i[0], "lon":i[1], "dt":"1609520400", "appid":"0ed702778efe831f0e3d546dc34eece3"}
         result = get_api(base, dic)
         weather_d[(i[0],i[1])] = result["results"]
     write_json(filename, weather_d)
-    # count function of sql
 
 def main():
-    ### cache_health_data("health_data.json")
-    loc_list = get_lat()
-    cache_weather(loc_list, "weather_data.json")
+    loc_list = get_lat("health_data_r.json", "health_data.json")
+    # cache_weather(loc_list, "weather_data.json")
 
 
 if __name__ == "__main__":
